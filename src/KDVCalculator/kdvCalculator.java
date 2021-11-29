8cee2a50d4ca720dff17a7fea29fe871b4496d93
Alper Ceviz <alperceviz41@gmail.com>
2021-11-29 22:13:06 +0300

    Update kdvCalculator.java
    kdvCalculator edited.

--- src/KDVCalculator/kdvCalculator.java
+++ src/KDVCalculator/kdvCalculator.java
@@ -1,16 +1,16 @@
 package KDVCalculator;
-
 import java.util.Scanner;
 
 public class kdvCalculator {
     public static void main(String[] args) {
-        double price, kdvPrice, kdvPercent;
+        double price, addKdvPrice, kdvPercent, kdvPrice;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter product price:  ");
         price = (int)input.nextDouble();
         boolean kdvAmount = price > 1000;
         kdvPercent = kdvAmount ? 0.08 : 0.18;
-        kdvPrice = price * kdvPercent;
+        addKdvPrice = price * kdvPercent;
+        kdvPrice = addKdvPrice + price;
 
         System.out.println("Calculating KDV Price. Please wait...");
         System.out.println("Product KDV Price : " +kdvPrice);
