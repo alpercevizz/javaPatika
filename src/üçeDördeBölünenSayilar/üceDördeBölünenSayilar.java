9d463dced46fad4f211c77529decfadb9b55495f
Alper Ceviz <alperceviz41@gmail.com>
2021-12-11 23:43:39 +0300

    üceDördeBölünenSayilar updated.

--- src/üçeDördeBölünenSayilar/üceDördeBölünenSayilar.java
+++ src/üçeDördeBölünenSayilar/üceDördeBölünenSayilar.java
@@ -5,14 +5,21 @@ public class üceDördeBölünenSayilar {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int num;
+        int num, total = 0, count = 0;
         System.out.println("Enter a number: ");
         num = input.nextInt();
 
         for(int i = 0; i <= num; i++) {
+            if(i == 0) continue; // Döngü sırasında sıfır ortalamayı etkileyeceği için ortalamaya katmamak gerekir.
+
             if(i % 3 == 0 && i % 4 == 0) {
-                System.out.print(i + " , ");
+                total +=i;
+                count++;
+
             }
         }
+
+        System.out.println("Average: " + total / count);
+
     }
 }
